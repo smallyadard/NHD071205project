69a79c386854f722830ebe78cb868cf9c3684f51
smallyadard <cutoibotcanh@gmail.com>
2025-11-18 23:54:25 +0700

    add pj rpg game

--- Student Management System/admin/footer.php
+++
@@ -1,7 +0,0 @@
-</div> <!-- Đóng thẻ div main -->
-    </div> <!-- Đóng thẻ div wrapper -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/script.js"></script>
-</body>
-</html>
